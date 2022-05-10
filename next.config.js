ad997a8040555c31d015a53f1322e9d81f3d1dd5
Anthony Mittaz <sync@me.com>
2022-05-10 10:20:18 +1000

    chore: tweak next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const relay = require('./relay.config.json');
 
 module.exports = {
+  swcMinify: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
   compiler: {
     relay: {
       src: relay.src,
